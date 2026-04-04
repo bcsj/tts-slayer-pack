e95c7706e853f08bc60e57d985052d5adc183248
Bjørn Jensen <bjornjensen1990@gmail.com>
2026-04-04 16:37:47 +0200

    moved createLabel to separate file

--- monsters.lua
+++ monsters.lua
@@ -1,3 +1,4 @@
+require("slayer-pack/ui-create-label")
 
 function onLoad()
 
@@ -148,25 +149,6 @@ function onLoad()
         end)
     end
     -----------------------------------------------------------------------------------
-end
-
-function createLabel()
-    self.UI.setXmlTable({
-        {
-            tag = "Text",
-            value = "Silbo's\nSlayer Pack",
-            attributes = {
-                color = "#0099ff",
-                outline = "White",
-                outlineSize = "3 3",
-                scale = ".14 .14 .14",
-                fontSize = "100",
-                position = "0 0 -60",
-                rotation = "180 180 0"
-            },
-            children = {}
-        }
-    })
 end
 
 function debug()
---
+++ ui-create-label.lua
@@ -0,0 +1,18 @@
+function createLabel()
+    self.UI.setXmlTable({
+        {
+            tag = "Text",
+            value = "Silbo's\nSlayer Pack",
+            attributes = {
+                color = "#0099ff",
+                outline = "White",
+                outlineSize = "3 3",
+                scale = ".14 .14 .14",
+                fontSize = "100",
+                position = "0 0 -60",
+                rotation = "180 180 0"
+            },
+            children = {}
+        }
+    })
+end
